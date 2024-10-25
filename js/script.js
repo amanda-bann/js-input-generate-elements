0ed84e70b0ac7c0ff4753d996c0d11f7c2683e53
amanda-bann <amanda_klap@protonmail.com>
2024-10-25 13:06:51 -0600

    First commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// add button
+const addShowButton = document.querySelector(".add");
+// label for the add button
+const showInput = document.querySelector(".add-show input");
+// unordered list (not yet visible)
+const showList = document.querySelector(".show-list");
+// span class for number of favorite shows
+const showCount = document.querySelector(".number");
